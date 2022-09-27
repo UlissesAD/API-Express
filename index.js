395d613dc26c0215e9ce036a7beebd538f9f0132
ulisses <uli.damasio>
2022-09-26 22:12:19 -0300

    commit body-parse ajust

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-Parser')
+const bodyParser = require('body-parser')
 
 const userRoute = require('./routes/userRoutes')
 const postRoute = require('./routes/postRoute')
